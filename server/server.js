5803bd519c21e64ff347deb3ad8316304fdec84f
VISHAL BALYAN <vbaliyan@researchilluminous.com>
2020-06-22 13:13:34 +0530

    JQuery and basic ui added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,33 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-  	from :'Admin',
-  	text : 'Welcome to the chat',
-  	createdAt : new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage',{
-  	from : 'Admin',
-  	text : 'New User Joined',
-  	createdAt : new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) =>{
-  	console.log('Message Created',message);
-
-  	
-  	io.emit('newMessage', {
-  		from : message.from,
-  		text : message.text,
-  		createdAt : new Date().getTime()
-  	});
-
-  	//socket.broadcast.emit('newMessage', {
-  	//	from : message.from,
-  	//	text : message.text,
-  	//	createdAt : new Date().getTime()
-  	//});
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
@@ -49,5 +36,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(	`Server is up on ${port}`);
+  console.log(`Server is up on ${port}`);
 });
